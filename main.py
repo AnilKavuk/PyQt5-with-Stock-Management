98f83f64a4d5ef9b0ac2eb429ef983f5bcd0a2d1
ANIL KAVUK <36512716+MrKavuk@users.noreply.github.com>
2023-10-01 00:22:40 +0300

    code review and bringing records

--- main.py
+++ main.py
@@ -39,82 +39,112 @@ MainWindow.show()
 
 
 def addProduct():
-    productName = ui.ProductName
-    productType = ui.ProductType
-    quantity = ui.ProductQuantity
-    quantityPrice = ui.ProductQuantityPrice
-    mountingType = ui.ProductMountingType
-    description = ui.ProductDescription
+    try:
+        productName = ui.ProductName
+        productType = ui.ProductType
+        quantity = ui.ProductQuantity
+        quantityPrice = ui.ProductQuantityPrice
+        mountingType = ui.ProductMountingType
+        description = ui.ProductDescription
 
-    if (
-        len(productName.text()) > 0
-        and len(productType.text()) > 0
-        and len(quantity.text()) > 0
-    ):
-        cursor.execute(
-            """INSERT INTO \
-            stock(productName,productType,quantity,quantityPrice,mountingType,description)\
-            VALUES (?,?,?,?,?,?)""",
-            (
-                productName.text(),
-                productType.text(),
-                quantity.text(),
-                quantityPrice.text(),
-                mountingType.currentText(),
-                description.toPlainText(),
-            ),
-        )
-        connection.commit()
-        productListing()
-    else:
-        ui.statusbar.showMessage(
-            "Product name, Product Type ya da Quantity boş bırakılamaz", 3000
-        )
+        if (
+            len(productName.text()) > 0
+            and len(productType.text()) > 0
+            and len(quantity.text()) > 0
+        ):
+            cursor.execute(
+                """INSERT INTO \
+                stock(productName,productType,quantity,quantityPrice,mountingType,description)\
+                VALUES (?,?,?,?,?,?)""",
+                (
+                    productName.text(),
+                    productType.text(),
+                    quantity.text(),
+                    quantityPrice.text(),
+                    mountingType.currentText(),
+                    description.toPlainText(),
+                ),
+            )
+            connection.commit()
+            ui.statusbar.showMessage("Saved successfully", 3000)
+            productListing()
+        else:
+            ui.statusbar.showMessage(
+                "Product name, Product Type or Quantity cannot be left blank", 3000
+            )
+    except Exception as error:
+        ui.statusbar.showMessage("This error was encountered: " + str(error), 3000)
 
 
 def productListing():
-    query = f"SELECT COUNT(*) FROM stock"
-    cursor.execute(query)
-    result = cursor.fetchone()
-    row_count = result[0]
-    ui.ProductTable.clear()
-    query = cursor.execute("SELECT * FROM stock")
-    ui.ProductTable.setRowCount(row_count)
-    productName = ui.ProductName
-    productType = ui.ProductType
-    quantity = ui.ProductQuantity
-    quantityPrice = ui.ProductQuantityPrice
-    mountingType = ui.ProductMountingType
-    description = ui.ProductDescription
-    ui.ProductTable.setHorizontalHeaderLabels(
-        (
-            "No",
-            "Product Name",
-            "Product Type",
-            "Quantity",
-            "Quantity Price",
-            "Mounting Type",
-            "Description",
-        )
-    )
-    ui.ProductTable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
-    for rowIndexes, rowValue in enumerate(query):
-        for columnIndexes, columnValue in enumerate(rowValue):
-            ui.ProductTable.setItem(
-                rowIndexes, columnIndexes, QtWidgets.QTableWidgetItem(str(columnValue))
+    try:
+        query = f"SELECT COUNT(*) FROM stock"
+        cursor.execute(query)
+        result = cursor.fetchone()
+        row_count = result[0]
+        ui.ProductTable.clear()
+        query = cursor.execute("SELECT * FROM stock")
+        ui.ProductTable.setRowCount(row_count)
+        productName = ui.ProductName
+        productType = ui.ProductType
+        quantity = ui.ProductQuantity
+        quantityPrice = ui.ProductQuantityPrice
+        mountingType = ui.ProductMountingType
+        description = ui.ProductDescription
+        ui.ProductTable.setHorizontalHeaderLabels(
+            (
+                "No",
+                "Product Name",
+                "Product Type",
+                "Quantity",
+                "Quantity Price",
+                "Mounting Type",
+                "Description",
             )
+        )
+        ui.ProductTable.horizontalHeader().setSectionResizeMode(QHeaderView.Stretch)
+        for rowIndexes, rowValue in enumerate(query):
+            for columnIndexes, columnValue in enumerate(rowValue):
+                ui.ProductTable.setItem(
+                    rowIndexes,
+                    columnIndexes,
+                    QtWidgets.QTableWidgetItem(str(columnValue)),
+                )
 
-    productName.clear(),
-    productType.clear(),
-    quantity.clear(),
-    quantityPrice.clear(),
-    mountingType.setCurrentIndex(0),
-    description.clear()
+        productName.clear(),
+        productType.clear(),
+        quantity.clear(),
+        quantityPrice.clear(),
+        mountingType.setCurrentIndex(0),
+        description.clear()
+
+    except Exception as error:
+        ui.statusbar.showMessage("This error was encountered: " + str(error), 3000)
 
 
 productListing()
+
+
+def bringRecords():
+    try:
+        selectedProduct = ui.ProductTable.selectedItems()
+        print(selectedProduct[1].text())
+
+        ui.ProductName.setText(selectedProduct[1].text())
+        ui.ProductType.setText(selectedProduct[2].text())
+        ui.ProductQuantity.setText(selectedProduct[3].text())
+        ui.ProductQuantityPrice.setText(selectedProduct[4].text())
+        ui.ProductMountingType.setCurrentText(selectedProduct[5].text())
+        ui.ProductDescription.setPlainText((selectedProduct[6].text()))
+
+    except Exception as error:
+        ui.statusbar.showMessage("This error was encountered: " + str(error), 3000)
+
+
 # ---------------------signal slot----------------------------------#
 ui.ProductAdd.clicked.connect(addProduct)
+
+ui.ProductTable.itemSelectionChanged.connect(bringRecords)
 # ---------------------signal slot----------------------------------#
 
 
